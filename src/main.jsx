02a97908b16dddeca8a3d5975b77b8b69f1fa297
Abdurahman Mohammed <157899943+Abdumk@users.noreply.github.com>
2025-03-23 16:41:00 +0300

    route fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
 // Code: Main entry point for the application
 // Description: This file is the main entry point for the application. It renders the App component inside a BrowserRouter component. The BrowserRouter component is used to provide routing capabilities to the application. The App component is the root component of the application and contains all the other components.
 
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this if deploying to a subpath (e.g., '/your-repo-name/')
+  base: './', // Change this if deploying to a subpath (e.g., '/your-repo-name/')
 });
